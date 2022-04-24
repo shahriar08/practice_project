789c625b7775be678e991b282a48a32822339892
AL-Shahriar <anik7744@gmail.com>
2022-04-25 04:27:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+if (JSON.parse(localStorage.getItem("task"))) {
+    var sData = JSON.parse(localStorage.getItem("task"));
+    for (i = 0; i < sData.length; i++) {
+        var row = document.getElementById("table").insertRow();
+        row.insertCell(0).innerHTML = sData[i].name;
+        row.insertCell(1).innerHTML = sData[i].age;
+        row.insertCell(2).innerHTML = sData[i].gender;
+        row.insertCell(3).innerHTML = sData[i].city;
+    }
+}
+
+// Store data to local storage
+function LocalDataStore() {
+    var fname = document.getElementById("name").value;
+    var lname = document.getElementById("age").value;
+    var gender = document.getElementById("gender").value;
+    var age = document.getElementById("city").value;
+
+    var taskData = {
+        name: name,
+        age: age,
+        gender: gender,
+        city: city,
+    };
+    if (!JSON.parse(localStorage.getItem("task"))) {
+        localStorage.setItem("task", JSON.stringify([taskData]));
+    } else {
+        var localStorageData = JSON.parse(localStorage.getItem("task"));
+        localStorageData.push(taskData);
+        localStorage.setItem("task", JSON.stringify(localStorageData));
+    }
+}
