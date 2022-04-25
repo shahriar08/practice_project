2fd29c8261eaf933e6789d68bd677d5df2a4ae62
shahriarlne <shahriar@thedoodleinc.com>
2022-04-25 10:28:33 +0600

    minnor changes

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-if (JSON.parse(localStorage.getItem("task"))) {
-    var sData = JSON.parse(localStorage.getItem("task"));
-    for (i = 0; i < sData.length; i++) {
-      var row = document.getElementById("table").insertRow();
-      row.insertCell(0).innerHTML = sData[i].name;
-      row.insertCell(1).innerHTML = sData[i].age;
-      row.insertCell(2).innerHTML = sData[i].gender;
-      row.insertCell(3).innerHTML = sData[i].city;
-    }
+//load data to localstorage
+if (JSON.parse(localStorage.getItem("data"))) {
+  var sData = JSON.parse(localStorage.getItem("data"));
+  for (i = 0; i < sData.length; i++) {
+    var row = document.getElementById("table").insertRow();
+    row.insertCell(0).innerHTML = sData[i].name;
+    row.insertCell(1).innerHTML = sData[i].age;
+    row.insertCell(2).innerHTML = sData[i].gender;
+    row.insertCell(3).innerHTML = sData[i].city;
   }
+}
 
-  // Store data to local storage
-  function LocalDataStore() {
-    var name = document.getElementById("name").value;
-    var age = document.getElementById("age").value;
-    var gender = document.getElementById("gender").value;
-    var city = document.getElementById("city").value;
+// Set data to localstorage
+function LocalDataStore() {
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var gender = document.getElementById("gender").value;
+  var city = document.getElementById("city").value;
 
-    var taskData = {
-      name: name,
-      age: age,
-      gender: gender,
-      city: city,
-    };
-    if (!JSON.parse(localStorage.getItem("task"))) {
-      localStorage.setItem("task", JSON.stringify([taskData]));
-    } else {
-      var localStorageData = JSON.parse(localStorage.getItem("task"));
-      localStorageData.push(taskData);
-      localStorage.setItem("task", JSON.stringify(localStorageData));
-    }
+  var dataData = {
+    name: name,
+    age: age,
+    gender: gender,
+    city: city,
+  };
+  if (!JSON.parse(localStorage.getItem("data"))) {
+    localStorage.setItem("data", JSON.stringify([dataData]));
+  } else {
+    var localStorageData = JSON.parse(localStorage.getItem("data"));
+    localStorageData.push(dataData);
+    localStorage.setItem("data", JSON.stringify(localStorageData));
   }
+}
